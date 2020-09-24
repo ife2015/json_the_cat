fe36a39988a49dfca82b3e2f9044eb274b4cd14f
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-24 22:40:26 +0000

    breedFetcher Test

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,35 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Sib', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc);
+
+      done();
+    });
+  });
+
+  it('returns "breed not found" description for an invalid/non-existent breed, via callback', (done) => {
+    fetchBreedDescription('scooby', (err, desc) => {
+
+      // we expect an error for this scenario
+      const expectedErr = "breed not found";
+      
+      assert.equal(err, expectedErr);
+
+      // compare returned description
+      assert.equal(desc, null);
+
+      done();
+    });
+  });
+
+
+});
